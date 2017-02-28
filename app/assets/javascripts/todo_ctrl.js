ab26caf5adc8c8c35ee7ab75109470c9dbc11a75
Michael Yatco <mjyatco@gmail.com>
2017-02-27 20:46:26 -0500

    Added ng-class, scss, function to count incomplete tasks, & button/method to delete completed tasks

--- app/assets/javascripts/todo_ctrl.js
+++ app/assets/javascripts/todo_ctrl.js
@@ -6,17 +6,42 @@
     // $scope.purpleHippo = "Buy coffee";
     // $scope.yellowHippo = "Grind coffee";
     // $scope.coffeeHippo = "Drink coffee!"
-    $scope.hippos = ["Buy coffee", "Grind coffee", "Drink coffee"];
+    $scope.hippos = [{task: "Buy coffee", 
+                      completed: false},
+                      {task: "Grind coffee", 
+                      completed: false},
+                      {task: "Drink coffee",
+                      completed: false}
+                      ];
 
-    $scope.addHippo = function(hippo) {
-      if (hippo) {
+    $scope.addHippo = function(newTask) {
+      var hippo = {task: newTask, completed: false};
+      if (newTask) {
         $scope.hippos.push(hippo);
-        $scope.newHippo = null;
+        $scope.newTask = null;
       }
     };
 
-    $scope.removeHippo = function(index) {
-      $scope.hippos.splice(index, 1);
+    $scope.toggleTask = function(hippo) {
+      hippo.completed = !hippo.completed;
+    };
+
+    $scope.countTasks = function() {
+      var counter = 0;
+      for (var i = 0; i < $scope.hippos.length; i++) {
+        if ($scope.hippos[i].completed === false) {
+          counter += 1;
+        }
+      }
+      return counter;
+    };
+
+    $scope.removeCompleted = function() {
+      for (var i = 0; i < $scope.hippos.length; i++) {
+        if ($scope.hippos[i].completed === true) {
+          $scope.hippos.splice(i, 1);
+        }
+      }
     };
   });
 }());
